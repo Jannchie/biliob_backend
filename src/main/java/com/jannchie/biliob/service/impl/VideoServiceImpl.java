2698be26994a2f7bd234589806a5428e05fad26f
Jannchie <jannchie@gmail.com>
2019-02-18 17:00:15 +0800

    feature: redis.

--- src/main/java/com/jannchie/biliob/service/impl/VideoServiceImpl.java
+++ src/main/java/com/jannchie/biliob/service/impl/VideoServiceImpl.java
@@ -1,7 +1,6 @@
 package com.jannchie.biliob.service.impl;
 
 import com.jannchie.biliob.constant.PageSizeEnum;
-import com.jannchie.biliob.constant.ResultEnum;
 import com.jannchie.biliob.exception.UserAlreadyFavoriteVideoException;
 import com.jannchie.biliob.model.Video;
 import com.jannchie.biliob.repository.UserRepository;
@@ -9,10 +8,12 @@ import com.jannchie.biliob.repository.VideoRepository;
 import com.jannchie.biliob.service.UserService;
 import com.jannchie.biliob.service.VideoService;
 import com.jannchie.biliob.utils.Message;
-import com.jannchie.biliob.utils.Result;
+import com.jannchie.biliob.utils.MySlice;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
@@ -32,6 +33,7 @@ import static org.springframework.data.mongodb.core.query.Criteria.where;
  * @author jannchie
  */
 @Service
+@CacheConfig(cacheNames = "videoService")
 public class VideoServiceImpl implements VideoService {
 	private static final Logger logger = LogManager.getLogger(VideoServiceImpl.class);
 	private final VideoRepository respository;
@@ -66,34 +68,36 @@ public class VideoServiceImpl implements VideoService {
 	}
 
 	@Override
-	public Slice<Video> getVideo(Long aid, String text, Integer page, Integer pagesize) {
+  @Cacheable(value = "author_slice", key = "#aid + #text + #page + #pagesize")
+  public MySlice<Video> getVideo(Long aid, String text, Integer page, Integer pagesize) {
     if(pagesize > PageSizeEnum.BIG_SIZE.getValue()){
       pagesize = PageSizeEnum.BIG_SIZE.getValue();
     }
 		if (!(aid == -1)) {
 			logger.info(aid);
-			return respository.searchByAid(
-					aid, PageRequest.of(page, pagesize, new Sort(Sort.Direction.DESC, "cView")));
-		} else if (!Objects.equals(text, "")) {
+      return new MySlice<>(respository.searchByAid(
+          aid, PageRequest.of(page, pagesize, new Sort(Sort.Direction.DESC, "cView"))));
+    } else if (!Objects.equals(text, "")) {
 			logger.info(text);
-			return respository.searchByText(
-					text, PageRequest.of(page, pagesize, new Sort(Sort.Direction.DESC, "cView")));
-		} else {
+      return new MySlice<>(respository.searchByText(
+          text, PageRequest.of(page, pagesize, new Sort(Sort.Direction.DESC, "cView"))));
+    } else {
 			logger.info("获取全部视频数据");
-			return respository.findAllByDataIsNotNull(
-					PageRequest.of(page, pagesize, new Sort(Sort.Direction.DESC, "cView")));
-		}
+      return new MySlice<>(respository.findAllByDataIsNotNull(
+          PageRequest.of(page, pagesize, new Sort(Sort.Direction.DESC, "cView"))));
+    }
 	}
 
 	@Override
-	public Slice<Video> getAuthorOtherVideo(Long aid, Long mid, Integer page, Integer pagesize) {
+  @Cacheable(value = "author_other_video", key = "#aid + #mid + #page + #pagesize")
+  public MySlice<Video> getAuthorOtherVideo(Long aid, Long mid, Integer page, Integer pagesize) {
     if(pagesize > PageSizeEnum.SMALL_SIZE.getValue()){
       pagesize = PageSizeEnum.SMALL_SIZE.getValue();
     }
     logger.info("获取作者其他数据");
-    return respository.findAuthorOtherVideo(
-        aid, mid, PageRequest.of(page, pagesize, new Sort(Sort.Direction.DESC, "cView")));
-	}
+    return new MySlice<>(respository.findAuthorOtherVideo(
+        aid, mid, PageRequest.of(page, pagesize, new Sort(Sort.Direction.DESC, "cView"))));
+  }
 
   /**
    * Get author top video.
@@ -102,12 +106,13 @@ public class VideoServiceImpl implements VideoService {
    * @param page     no use
    * @param pagesize the number of displayed video
    * @param sort 0: order by view || 1: order by publish datetime.
-   * @return slice of author's video
+   * @return slice of author's video || null: param error
    */
   @Override
-  public ResponseEntity getAuthorTopVideo(Long mid, Integer page, Integer pagesize, Integer sort) {
+  @Cacheable(value = "author_top_video", key = "#mid + #page + #pagesize + #sort")
+  public MySlice<Video> getAuthorTopVideo(Long mid, Integer page, Integer pagesize, Integer sort) {
     if (pagesize >= MAX_PAGE_SIZE) {
-      return new ResponseEntity<>(new Result(ResultEnum.PARAM_ERROR), HttpStatus.BAD_REQUEST);
+      return null;
     }
     Sort videoSort;
     if (Objects.equals(sort, VIEW_COUNT.getValue())) {
@@ -115,13 +120,13 @@ public class VideoServiceImpl implements VideoService {
     } else if (Objects.equals(sort, PUBLISH_TIME.getValue())) {
       videoSort = new Sort(Sort.Direction.DESC, "datetime");
     } else {
-      return new ResponseEntity<>(new Result(ResultEnum.PARAM_ERROR), HttpStatus.BAD_REQUEST);
+      return null;
     }
 
-    Slice video = respository.findAuthorTopVideo(
+    Slice<Video> video = respository.findAuthorTopVideo(
         mid, PageRequest.of(page, pagesize, videoSort));
     logger.info("获取mid:{} 播放最多的视频", mid);
-    return new ResponseEntity<>(video, HttpStatus.OK);
+    return new MySlice<>(video);
   }
 
   /**
@@ -130,11 +135,12 @@ public class VideoServiceImpl implements VideoService {
    * @return the latest of my video
    */
   @Override
-  public ResponseEntity<Video> getMyVideo() {
+  @Cacheable(value = "my", key = "'latest'")
+  public Video getMyVideo() {
     Query q = new Query(where("mid").is(1850091)).with(new Sort(Sort.Direction.DESC, "datetime"));
     q.fields().exclude("data");
     Video video = mongoTemplate.findOne(q, Video.class);
     logger.info("获取广告");
-    return new ResponseEntity<>(video, HttpStatus.OK);
+    return video;
   }
 }
