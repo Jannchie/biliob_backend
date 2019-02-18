99902f020c87b4b41a4c5f136df42c43e70a2938
Jannchie <jannchie@gmail.com>
2019-02-19 00:21:43 +0800

    fix: scheduling

--- src/main/java/com/jannchie/biliob/BiliobApplication.java
+++ src/main/java/com/jannchie/biliob/BiliobApplication.java
@@ -3,12 +3,14 @@ package com.jannchie.biliob;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author jannchie
  */
-@SpringBootApplication
 @EnableCaching
+@EnableScheduling
+@SpringBootApplication
 public class BiliobApplication {
 
 	public static void main(String[] args) {
