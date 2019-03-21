6a086091dc5c0783fac50029d664ab36600f5bac
Jannchie <jannchie@gmail.com>
2019-03-21 16:22:01 +0800

    feature: user operation with feedback

---
+++ src/main/java/com/jannchie/biliob/utils/credit/CheckInCreditCalculator.java
@@ -0,0 +1,58 @@
+package com.jannchie.biliob.utils.credit;
+
+import com.jannchie.biliob.constant.ExceptionEnum;
+import com.jannchie.biliob.constant.ResultEnum;
+import com.jannchie.biliob.exception.BusinessException;
+import com.jannchie.biliob.model.CheckIn;
+import com.jannchie.biliob.model.User;
+import com.jannchie.biliob.utils.Result;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+
+/** @author jannchie */
+@Component
+public class CheckInCreditCalculator extends AbstractCreditCalculator {
+
+  private final MongoTemplate mongoTemplate;
+
+  @Autowired
+  public CheckInCreditCalculator(MongoTemplate mongoTemplate) {
+    super(mongoTemplate);
+    this.mongoTemplate = mongoTemplate;
+  }
+
+  @Override
+  public void execute(User user, ObjectId objectId) {
+    Boolean isCheckedIn =
+        mongoTemplate.exists(new Query(where("name").is(user.getName())), "check_in");
+    String userName = user.getName();
+    Integer credit = user.getCredit();
+    if (isCheckedIn) {
+      throw new BusinessException(ExceptionEnum.ALREADY_SIGNED);
+    } else {
+      // 插入已签到集合
+      mongoTemplate.insert(new CheckIn(userName), "check_in");
+
+      // update execute status
+      Query query = new Query(where("_id").is(objectId));
+      Update update = new Update();
+      update.set("isExecuted", true);
+      mongoTemplate.updateFirst(query, update, "user_record");
+    }
+  }
+
+  @Override
+  ResponseEntity getResponseEntity(HashMap data) {
+    return new ResponseEntity<>(new Result(ResultEnum.SIGN_SUCCEED, data), HttpStatus.OK);
+  }
+}
