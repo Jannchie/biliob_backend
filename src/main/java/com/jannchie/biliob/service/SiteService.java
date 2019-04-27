6121c16c53f205847237f69fa330d6639b937bdd
Jannchie <jannchie@gmail.com>
2019-04-27 15:41:33 +0800

    feature: get observing objects count

--- src/main/java/com/jannchie/biliob/service/SiteService.java
+++ src/main/java/com/jannchie/biliob/service/SiteService.java
@@ -3,6 +3,8 @@ package com.jannchie.biliob.service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 /** @author jannchie */
 @Service
 public interface SiteService {
@@ -14,4 +16,12 @@ public interface SiteService {
    * @return Online number.
    */
   ResponseEntity listOnline(Integer days);
+
+  /**
+   * Get the data of the number of video and author being observed. Get number of observers.
+   *
+   * @return The data of the number of video and author being observed and get the number of
+   *     observers.
+   */
+  Map getBiliOBCounter();
 }
