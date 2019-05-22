50b0ed21f60b61d2f422d59a263a2c698ef63484
Jannchie <jannchie@gmail.com>
2019-05-22 17:27:23 +0800

    update: pom, test

--- src/test/java/com/jannchie/biliob/RedisTests.java
+++ src/test/java/com/jannchie/biliob/RedisTests.java
@@ -27,18 +27,6 @@ public class RedisTests {
 
   @Test
   @Transactional
-  public void testURL() {
-    String testString = "https://api.bilibili.com/x/web-interface/card?mid=1850091";
-    stringRedisTemplate
-        .opsForList()
-        .leftPush(
-            "authorRedis:start_urls", "https://api.bilibili.com/x/web-interface/card?mid=1850091");
-    String s = stringRedisTemplate.opsForList().leftPop("authorRedis:start_urls");
-    Assert.assertEquals("URL添加与删除测试失败", testString, s);
-  }
-
-  @Test
-  @Transactional
   public void getAuthorQueueLength() {
     Long authorCrawlTaskQueueLength = redisOps.getAuthorQueueLength();
     Assert.assertEquals(
