696c2938b6b7413983385c49c70190b270fa4e5c
Jannchie <jannchie@gmail.com>
2019-05-30 22:04:38 +0800

    feature: keyword of site

--- src/main/java/com/jannchie/biliob/service/VideoService.java
+++ src/main/java/com/jannchie/biliob/service/VideoService.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /** @author jannchie */
@@ -111,7 +112,7 @@ public interface VideoService {
    *
    * @return
    */
-  Map getPopularKeyword();
+  List getPopularKeyword();
 
   /**
    * get guest prefer keyword
