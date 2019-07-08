2c153edffb74295197a841b5a3ab5fbede58a9d1
jannchie <jannchie@gmail.com>
2019-07-08 21:20:43 +0900

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-pid=$(ps -ef | grep biliob | grep .jar | grep -v grep | awk '{print $2}')if [ -n $pid ]then  kill -9 $pidfisource /etc/profilecd ~/biliob_backendnohup java -jar ./target/biliob*.jar >log.out 2>&1 &
+#!/usr/bin/env bashpid=$(ps -ef | grep biliob | grep .jar | grep -v grep | awk '{print $2}')if [[ -n ${pid} ]]then  kill -9 ${pid}fisource /etc/profilecd ~/biliob_backendnohup java -jar ./target/biliob*.jar >log.out 2>&1 &
