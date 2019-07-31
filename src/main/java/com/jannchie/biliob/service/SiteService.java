346277f94ba3c464be792a281823322026169e9e
jannchie <jannchie@gmail.com>
2019-07-31 21:00:34 +0900

    feature: site online grouped data

--- src/main/java/com/jannchie/biliob/service/SiteService.java
+++ src/main/java/com/jannchie/biliob/service/SiteService.java
@@ -3,46 +3,56 @@ package com.jannchie.biliob.service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
-/** @author jannchie */
+/**
+ * @author jannchie
+ */
 @Service
 public interface SiteService {
 
-  /**
-   * Get the data of the number of people watching video on bilibili.
-   *
-   * @param days The days of data that this method should return.
-   * @return Online number.
-   */
-  ResponseEntity listOnline(Integer days);
+    /**
+     * Get the data of the number of people watching video on bilibili.
+     *
+     * @param days The days of data that this method should return.
+     * @return Online number.
+     */
+    ResponseEntity listOnline(Integer days);
 
-  /**
-   * Get the data of the number of video and author being observed. Get number of observers.
-   *
-   * @return The data of the number of video and author being observed and get the number of
-   *     observers.
-   */
-  Map getBiliOBCounter();
+    /**
+     * Get grouped data of bilibili
+     *
+     * @return Online grouped data.
+     */
+    ResponseEntity<List> getGroupedOnlineData();
 
-  /**
-   * Get the number of author be observed.
-   *
-   * @return the number of author be observed.
-   */
-  Map getAuthorCount();
+    /**
+     * Get the data of the number of video and author being observed. Get number of observers.
+     *
+     * @return The data of the number of video and author being observed and get the number of
+     * observers.
+     */
+    Map getBiliOBCounter();
 
-  /**
-   * Get the number of video be observed.
-   *
-   * @return the number of video be observed.
-   */
-  Map getVideoCount();
+    /**
+     * Get the number of author be observed.
+     *
+     * @return the number of author be observed.
+     */
+    Map getAuthorCount();
 
-  /**
-   * Get the number of user be observed.
-   *
-   * @return the number of user be observed.
-   */
-  Map getUserCount();
+    /**
+     * Get the number of video be observed.
+     *
+     * @return the number of video be observed.
+     */
+    Map getVideoCount();
+
+    /**
+     * Get the number of user be observed.
+     *
+     * @return the number of user be observed.
+     */
+    Map getUserCount();
 }
