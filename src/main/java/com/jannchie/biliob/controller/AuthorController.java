ddf5d5bccc90fdb67dedcbf292a626e9342004c2
jannchie <jannchie@gmail.com>
2019-08-29 21:10:34 +0900

    feature: refresh top author

--- src/main/java/com/jannchie/biliob/controller/AuthorController.java
+++ src/main/java/com/jannchie/biliob/controller/AuthorController.java
@@ -14,76 +14,88 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-/** @author jannchie */
+/**
+ * @author jannchie
+ */
 @RestController
 public class AuthorController {
 
-  private final AuthorService authorService;
+    private final AuthorService authorService;
 
-  @Autowired
-  public AuthorController(AuthorService authorService) {
-    this.authorService = authorService;
-  }
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
 
-  @RequestMapping(method = RequestMethod.GET, value = "/api/author/{mid}")
-  public Author getAuthorDetails(
-      @PathVariable("mid") Long mid, @RequestParam(defaultValue = "1") Integer type) {
-    return authorService.getAuthorDetails(mid, type);
-  }
+    @RequestMapping(method = RequestMethod.GET, value = "/api/author/{mid}")
+    public Author getAuthorDetails(
+            @PathVariable("mid") Long mid, @RequestParam(defaultValue = "1") Integer type) {
+        return authorService.getAuthorDetails(mid, type);
+    }
 
-  @RequestMapping(method = RequestMethod.POST, value = "/api/author")
-  public ResponseEntity<Message> postAuthorByMid(@RequestBody @Valid Long mid)
-      throws UserAlreadyFavoriteAuthorException, AuthorAlreadyFocusedException {
-    authorService.postAuthorByMid(mid);
-    return new ResponseEntity<>(new Message(200, "观测UP主成功"), HttpStatus.OK);
-  }
+    @RequestMapping(method = RequestMethod.POST, value = "/api/author")
+    public ResponseEntity<Message> postAuthorByMid(@RequestBody @Valid Long mid)
+            throws UserAlreadyFavoriteAuthorException, AuthorAlreadyFocusedException {
+        authorService.postAuthorByMid(mid);
+        return new ResponseEntity<>(new Message(200, "观测UP主成功"), HttpStatus.OK);
+    }
 
-  @RequestMapping(method = RequestMethod.GET, value = "/api/author")
-  public MySlice<Author> getAuthor(
-      @RequestParam(defaultValue = "0") Integer sort,
-      @RequestParam(defaultValue = "0") Integer page,
-      @RequestParam(defaultValue = "20") Integer pageSize,
-      @RequestParam(defaultValue = "-1") Long mid,
-      @RequestParam(defaultValue = "") String text) {
-    return authorService.getAuthor(mid, text, page, pageSize, sort);
-  }
+    @RequestMapping(method = RequestMethod.GET, value = "/api/author")
+    public MySlice<Author> getAuthor(
+            @RequestParam(defaultValue = "0") Integer sort,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "20") Integer pageSize,
+            @RequestParam(defaultValue = "-1") Long mid,
+            @RequestParam(defaultValue = "") String text) {
+        return authorService.getAuthor(mid, text, page, pageSize, sort);
+    }
 
-  @RequestMapping(method = RequestMethod.GET, value = "/api/author/{mid}/info")
-  public Author getAuthorInfo(@PathVariable("mid") Long mid) {
-    return authorService.getAuthorInfo(mid);
-  }
+    @RequestMapping(method = RequestMethod.GET, value = "/api/author/{mid}/info")
+    public Author getAuthorInfo(@PathVariable("mid") Long mid) {
+        return authorService.getAuthorInfo(mid);
+    }
 
-  @RequestMapping(method = RequestMethod.GET, value = "/api/rank/fans-increase-rate")
-  public ResponseEntity listFansIncreaseRate() {
-    return authorService.listFansIncreaseRate();
-  }
+    @RequestMapping(method = RequestMethod.GET, value = "/api/rank/fans-increase-rate")
+    public ResponseEntity listFansIncreaseRate() {
+        return authorService.listFansIncreaseRate();
+    }
 
-  @RequestMapping(method = RequestMethod.GET, value = "/api/rank/fans-decrease-rate")
-  public ResponseEntity listFansDecreaseRate() {
-    return authorService.listFansDecreaseRate();
-  }
+    @RequestMapping(method = RequestMethod.GET, value = "/api/rank/fans-decrease-rate")
+    public ResponseEntity listFansDecreaseRate() {
+        return authorService.listFansDecreaseRate();
+    }
 
-  @RequestMapping(method = RequestMethod.GET, value = "/api/author/{mid}/fans-rate")
-  public ResponseEntity listFansRate(@PathVariable("mid") Long mid) {
-    return authorService.listFansRate(mid);
-  }
+    @RequestMapping(method = RequestMethod.GET, value = "/api/author/{mid}/fans-rate")
+    public ResponseEntity listFansRate(@PathVariable("mid") Long mid) {
+        return authorService.listFansRate(mid);
+    }
 
-  @RequestMapping(method = RequestMethod.GET, value = "/api/author/real-time")
-  public ResponseEntity listRealTime(
-      @RequestParam(defaultValue = "0") Long aMid, @RequestParam(defaultValue = "0") Long bMid) {
-    return authorService.getRealTimeData(aMid, bMid);
-  }
+    @RequestMapping(method = RequestMethod.GET, value = "/api/author/real-time")
+    public ResponseEntity listRealTime(
+            @RequestParam(defaultValue = "0") Long aMid, @RequestParam(defaultValue = "0") Long bMid) {
+        return authorService.getRealTimeData(aMid, bMid);
+    }
 
-  @RequestMapping(method = RequestMethod.GET, value = "/api/author/tag")
-  public List listAuthorTag(
-      @RequestParam(defaultValue = "0") Long mid,
-      @RequestParam(defaultValue = "10") Integer limit) {
-    return authorService.listAuthorTag(mid, limit);
-  }
+    @RequestMapping(method = RequestMethod.GET, value = "/api/author/top")
+    public ResponseEntity getTopAuthor() {
+        return authorService.getTopAuthor();
+    }
 
-  @RequestMapping(method = RequestMethod.GET, value = "/api/author/{mid}/relationship")
-  public List listRelateAuthor(
-      @PathVariable("mid") Long mid, @RequestParam(defaultValue = "10") Integer limit) {
-    return authorService.listRelatedAuthorByMid(mid, limit);
-  }
+    @RequestMapping(method = RequestMethod.GET, value = "/api/author/top/refresh")
+    public ResponseEntity getLatestTopAuthorData() {
+        return authorService.getLatestTopAuthorData();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/api/author/tag")
+    public List listAuthorTag(
+            @RequestParam(defaultValue = "0") Long mid,
+            @RequestParam(defaultValue = "10") Integer limit) {
+        return authorService.listAuthorTag(mid, limit);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/api/author/{mid}/relationship")
+    public List listRelateAuthor(
+            @PathVariable("mid") Long mid, @RequestParam(defaultValue = "10") Integer limit) {
+        return authorService.listRelatedAuthorByMid(mid, limit);
+    }
 }
--- src/main/java/com/jannchie/biliob/utils/TracerScheduler.java
+++ src/main/java/com/jannchie/biliob/utils/TracerScheduler.java
@@ -1,8 +1,12 @@
 package com.jannchie.biliob.utils;
 
+import com.jannchie.biliob.model.Author;
 import com.jannchie.biliob.model.ScheduleItem;
 import com.jannchie.biliob.model.TracerTask;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -10,93 +14,123 @@ import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
-/** @author jannchie */
+/**
+ * @author jannchie
+ */
 @Component
 public class TracerScheduler {
-  private static final Integer DEAD_MINUTES = -5;
-  private final MongoTemplate mongoTemplate;
-  private final RedisOps redisOps;
+    private static final Integer DEAD_MINUTES = -5;
+    private static final Logger logger = LogManager.getLogger();
+    private final MongoTemplate mongoTemplate;
+    private final RedisOps redisOps;
 
-  @Autowired
-  public TracerScheduler(MongoTemplate mongoTemplate, RedisOps redisOps) {
-    this.mongoTemplate = mongoTemplate;
-    this.redisOps = redisOps;
-  }
-
-  @Scheduled(cron = "0 0/5 * * * ?")
-  public void checkDeadTask() {
-    Date deadDate = getDeadDate();
-    mongoTemplate.updateMulti(
-        Query.query(
-            Criteria.where("update_time").lt(deadDate).and("status").ne(TracerStatus.FINISHED)),
-        Update.update("status", TracerStatus.DEAD).set("msg", "该任务已离线"),
-        TracerTask.class);
-  }
-
-  @Scheduled(cron = "0 0/5 * * * ?")
-  public void addCustomCrawlTaskEvery5Min() {
-    postCustomVideoCrawlSchedule(3);
-    postCustomAuthorCrawlSchedule(3);
-  }
-
-  @Scheduled(cron = "0 0 0/1 * * ?")
-  public void addCustomCrawlTaskEvery1Hour() {
-    postCustomVideoCrawlSchedule(2);
-    postCustomAuthorCrawlSchedule(2);
-  }
-
-  @Scheduled(cron = "0 0 0/6 * * ?")
-  public void addCustomCrawlTaskEvery6Hour() {
-    postCustomVideoCrawlSchedule(1);
-    postCustomAuthorCrawlSchedule(1);
-  }
-
-  private void postCustomVideoCrawlSchedule(Integer frequency) {
-    List<ScheduleItem> videoScheduleList =
-        mongoTemplate.find(
-            Query.query(Criteria.where("frequency").is(frequency).and("type").is("video")),
-            ScheduleItem.class,
-            "crawl_schedule");
-    for (ScheduleItem item : videoScheduleList) {
-      List<String> ids = item.getIdList();
-      for (String id : ids) {
-        redisOps.postVideoCrawlTask(id);
-      }
+    @Autowired
+    public TracerScheduler(MongoTemplate mongoTemplate, RedisOps redisOps) {
+        this.mongoTemplate = mongoTemplate;
+        this.redisOps = redisOps;
     }
-  }
 
-  private void postCustomAuthorCrawlSchedule(Integer frequency) {
-    List<ScheduleItem> authorScheduleList =
-        mongoTemplate.find(
-            Query.query(Criteria.where("frequency").is(frequency).and("type").is("author")),
-            ScheduleItem.class,
-            "crawl_schedule");
-    for (ScheduleItem item : authorScheduleList) {
-      List<String> ids = item.getIdList();
-      for (String id : ids) {
-        redisOps.postAuthorCrawlTask(id);
-      }
+    @Scheduled(cron = "0 0/1 * * * ?")
+    public void recordTop5Author() {
+        logger.info("每分钟爬取前3名作者的数据");
+        List<Author> authors = mongoTemplate.find(new Query().with(Sort.by(Sort.Direction.DESC, "cFans")).limit(3), Author.class, "author");
+        for (Author author : authors) {
+            Long mid = author.getMid();
+            redisOps.postAuthorCrawlTask(mid);
+        }
     }
-  }
 
-  Date getDeadDate() {
-    Calendar c = Calendar.getInstance();
-    c.add(Calendar.MINUTE, TracerScheduler.DEAD_MINUTES);
-    c.add(Calendar.HOUR, 8);
-    return c.getTime();
-  }
+    @Scheduled(cron = "0 0/5 * * * ?")
+    public void recordSpiderQueueStatus() {
+        logger.info("记录爬虫队列状态");
+        Long authorQueueLength = redisOps.getAuthorQueueLength();
+        Long videoQueueLength = redisOps.getVideoQueueLength();
+        Date date = Calendar.getInstance().getTime();
+        Map data = new HashMap<String, Object>() {{
+            put("date", date);
+            put("author", authorQueueLength);
+            put("video", videoQueueLength);
+        }};
+        mongoTemplate.insert(data, "spider_queue_status");
+    }
 
-  private static class TracerStatus {
-    static final Integer START = 1;
-    static final Integer UPDATE = 2;
-    static final Integer DEAD = 4;
-    static final Integer ALIVE = 5;
-    static final Integer WARNING = 6;
-    static final Integer TIMEOUT = 8;
-    static final Integer FINISHED = 9;
-  }
+    @Scheduled(cron = "0 0/5 * * * ?")
+    public void checkDeadTask() {
+        logger.info("检查死亡爬虫");
+        Date deadDate = getDeadDate();
+        mongoTemplate.updateMulti(
+                Query.query(
+                        Criteria.where("update_time").lt(deadDate).and("status").ne(TracerStatus.FINISHED)),
+                Update.update("status", TracerStatus.DEAD).set("msg", "该任务已离线"),
+                TracerTask.class);
+    }
+
+    @Scheduled(cron = "0 0/5 * * * ?")
+    public void addCustomCrawlTaskEvery5Min() {
+        logger.info("执行每5分钟的爬虫任务");
+        postCustomVideoCrawlSchedule(3);
+        postCustomAuthorCrawlSchedule(3);
+    }
+
+    @Scheduled(cron = "0 0 0/1 * * ?")
+    public void addCustomCrawlTaskEvery1Hour() {
+        logger.info("执行每小时的爬虫任务");
+        postCustomVideoCrawlSchedule(2);
+        postCustomAuthorCrawlSchedule(2);
+    }
+
+    @Scheduled(cron = "0 0 0/6 * * ?")
+    public void addCustomCrawlTaskEvery6Hour() {
+        logger.info("执行每6小时的爬虫任务");
+        postCustomVideoCrawlSchedule(1);
+        postCustomAuthorCrawlSchedule(1);
+    }
+
+
+    private void postCustomVideoCrawlSchedule(Integer frequency) {
+        List<ScheduleItem> videoScheduleList =
+                mongoTemplate.find(
+                        Query.query(Criteria.where("frequency").is(frequency).and("type").is("video")),
+                        ScheduleItem.class,
+                        "crawl_schedule");
+        for (ScheduleItem item : videoScheduleList) {
+            List<String> ids = item.getIdList();
+            for (String id : ids) {
+                redisOps.postVideoCrawlTask(id);
+            }
+        }
+    }
+
+    private void postCustomAuthorCrawlSchedule(Integer frequency) {
+        List<ScheduleItem> authorScheduleList =
+                mongoTemplate.find(
+                        Query.query(Criteria.where("frequency").is(frequency).and("type").is("author")),
+                        ScheduleItem.class,
+                        "crawl_schedule");
+        for (ScheduleItem item : authorScheduleList) {
+            List<String> ids = item.getIdList();
+            for (String id : ids) {
+                redisOps.postAuthorCrawlTask(id);
+            }
+        }
+    }
+
+    Date getDeadDate() {
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.MINUTE, TracerScheduler.DEAD_MINUTES);
+        c.add(Calendar.HOUR, 8);
+        return c.getTime();
+    }
+
+    private static class TracerStatus {
+        static final Integer START = 1;
+        static final Integer UPDATE = 2;
+        static final Integer DEAD = 4;
+        static final Integer ALIVE = 5;
+        static final Integer WARNING = 6;
+        static final Integer TIMEOUT = 8;
+        static final Integer FINISHED = 9;
+    }
 }
