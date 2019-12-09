6cd334210b1ddf436cf64c20781967c489ddd788
jannchie <jannchie@gmail.com>
2019-12-09 22:17:00 +0900

    FIX: Do not catch localhost visit.

--- src/main/java/com/jannchie/biliob/utils/IpHandlerInterceptor.java
+++ src/main/java/com/jannchie/biliob/utils/IpHandlerInterceptor.java
@@ -20,81 +20,82 @@ import java.util.Objects;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
 
-/** @author jannchie */
+/**
+ * @author jannchie
+ */
 @Component
 public class IpHandlerInterceptor implements HandlerInterceptor {
 
-  private static final Logger logger = LogManager.getLogger(IpHandlerInterceptor.class);
-  private static final Integer MAX_CUD_IN_MINUTE = 180;
-  private static final Integer MAX_R_IN_MINUTE = 360;
-  private static final String IP = "ip";
-  private static HashMap<String, Integer> blackIP = new HashMap<>(256);
-  private final MongoTemplate mongoTemplate;
+    private static final Logger logger = LogManager.getLogger(IpHandlerInterceptor.class);
+    private static final Integer MAX_CUD_IN_MINUTE = 180;
+    private static final Integer MAX_R_IN_MINUTE = 360;
+    private static final String IP = "ip";
+    private static final String LOCALHOST = "0:0:0:0:0:0:0:1";
+    private static HashMap<String, Integer> blackIP = new HashMap<>(256);
+    private final MongoTemplate mongoTemplate;
 
-  public IpHandlerInterceptor(MongoTemplate mongoTemplate) {
-    this.mongoTemplate = mongoTemplate;
-  }
-
-  /** controller 执行之前调用 */
-  @Override
-  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-      throws Exception {
-
-    String ip = IpUtil.getIpAddress(request);
-
-    // 在黑名单中直接处决
-    if (mongoTemplate.exists(query(where(IP).is(ip)), Blacklist.class)) {
-      returnJson(response);
-      return false;
+    public IpHandlerInterceptor(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
     }
 
-    Integer limitCount = Integer.MAX_VALUE;
-    if (Objects.equals(request.getMethod(), HttpMethod.DELETE.name())
-        || Objects.equals(request.getMethod(), HttpMethod.POST.name())
-        || Objects.equals(request.getMethod(), HttpMethod.PATCH.name())
-        || Objects.equals(request.getMethod(), HttpMethod.PUT.name())) {
-      // POST或PATCH或PUT或DELETE方法
-      limitCount = MAX_CUD_IN_MINUTE;
-    } else if (Objects.equals(request.getMethod(), HttpMethod.GET.name())) {
-      // GET方法
-      limitCount = MAX_R_IN_MINUTE;
-    }
-    if (blackIP.containsKey(ip)) {
-      Integer newCount = blackIP.get(ip) + 1;
-      blackIP.put(ip, newCount);
-      if (newCount > limitCount) {
-        mongoTemplate.insert(new Blacklist(ip), "blacklist");
-        logger.info(ip);
-        blackIP.remove(ip);
-        response.setStatus(HttpStatus.FORBIDDEN.value());
-        returnJson(response);
-        return false;
-      }
-    } else {
-      blackIP.put(ip, 1);
-    }
-    return true;
-  }
+    /**
+     * controller 执行之前调用
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 
-  @Scheduled(cron = "0 0/1 * * * ?")
-  public void refresh() {
-    blackIP = new HashMap<>(256);
-  }
+        String ip = IpUtil.getIpAddress(request);
+        // Debugger
+        if (ip.equals(LOCALHOST)) {
+            return true;
+        }
+        // 在黑名单中直接处决
+        if (mongoTemplate.exists(query(where(IP).is(ip)), Blacklist.class)) {
+            returnJson(response);
+            return false;
+        }
 
-  private void returnJson(HttpServletResponse response) throws Exception {
-    PrintWriter writer = null;
-    response.setCharacterEncoding("UTF-8");
-    response.setContentType("application/json; charset=utf-8");
-    try {
-      writer = response.getWriter();
-      writer.print("{\"msg\":\"YOU HAVE BEEN CAUGHT.\"}");
-
-    } catch (IOException e) {
-      logger.error("response error", e);
-    } finally {
-      if (writer != null) {
-        writer.close();
-      }
+        Integer limitCount = Integer.MAX_VALUE;
+        if (Objects.equals(request.getMethod(), HttpMethod.DELETE.name())
+                || Objects.equals(request.getMethod(), HttpMethod.POST.name())
+                || Objects.equals(request.getMethod(), HttpMethod.PATCH.name())
+                || Objects.equals(request.getMethod(), HttpMethod.PUT.name())) {
+            // POST或PATCH或PUT或DELETE方法
+            limitCount = MAX_CUD_IN_MINUTE;
+        } else if (Objects.equals(request.getMethod(), HttpMethod.GET.name())) {
+            // GET方法
+            limitCount = MAX_R_IN_MINUTE;
+        }
+        if (blackIP.containsKey(ip)) {
+            Integer newCount = blackIP.get(ip) + 1;
+            blackIP.put(ip, newCount);
+            if (newCount > limitCount) {
+                mongoTemplate.insert(new Blacklist(ip), "blacklist");
+                logger.info(ip);
+                blackIP.remove(ip);
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                returnJson(response);
+                return false;
+            }
+        } else {
+            blackIP.put(ip, 1);
+        }
+        return true;
     }
-  }
+
+    @Scheduled(cron = "0 0/1 * * * ?")
+    public void refresh() {
+        blackIP = new HashMap<>(256);
+    }
+
+    private void returnJson(HttpServletResponse response) {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        try (PrintWriter writer = response.getWriter()) {
+            writer.print("{\"msg\":\"YOU HAVE BEEN CAUGHT.\"}");
+
+        } catch (IOException e) {
+            logger.error("response error", e);
+        }
+    }
 }
