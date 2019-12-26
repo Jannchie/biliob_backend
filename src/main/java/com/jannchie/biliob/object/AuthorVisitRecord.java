0620cc8ba572a6f6a44092d712eacd64d3485870
jannchie <jannchie@gmail.com>
2019-12-26 22:17:01 +0800

    FIX: Author visit top

---
+++ src/main/java/com/jannchie/biliob/object/AuthorVisitRecord.java
@@ -0,0 +1,31 @@
+package com.jannchie.biliob.object;
+
+public class AuthorVisitRecord {
+    private String name;
+    private Long mid;
+    private Integer count;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getMid() {
+        return mid;
+    }
+
+    public void setMid(Long mid) {
+        this.mid = mid;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
