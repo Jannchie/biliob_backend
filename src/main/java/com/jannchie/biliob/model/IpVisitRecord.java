3d50854100fe8254fec0b3a59e0c595b8a6647a8
jannchie <jannchie@gmail.com>
2020-01-22 02:47:35 +0900

    UPDATE: Visit watcher

--- src/main/java/com/jannchie/biliob/model/IpVisitRecord.java
+++ src/main/java/com/jannchie/biliob/model/IpVisitRecord.java
@@ -2,7 +2,6 @@ package com.jannchie.biliob.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -16,20 +15,29 @@ import java.util.Date;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class IpVisitRecord {
     @Id
-    private ObjectId id;
-
+    private String id;
+    private String groupBy;
     private String ip;
     private String userAgent;
+    private Integer count;
     private String uri;
-
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+0")
+    private Date lastTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+0")
+    private Date firstTime;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+0")
     private Date datetime;
+    private String lastUri;
+    private String lastUserAgent;
 
     public IpVisitRecord(String ip, String userAgent, String uri) {
         this.ip = ip;
         this.userAgent = userAgent;
         this.datetime = Calendar.getInstance().getTime();
         this.uri = uri;
+    }
+
+    public IpVisitRecord() {
     }
 
     public String getIp() {
@@ -62,5 +70,37 @@ public class IpVisitRecord {
 
     public void setUri(String uri) {
         this.uri = uri;
+    }
+
+    public String getLastUri() {
+        return lastUri;
+    }
+
+    public void setLastUri(String lastUri) {
+        this.lastUri = lastUri;
+    }
+
+    public String getLastUserAgent() {
+        return lastUserAgent;
+    }
+
+    public void setLastUserAgent(String lastUserAgent) {
+        this.lastUserAgent = lastUserAgent;
+    }
+
+    public String getGroupBy() {
+        return groupBy;
+    }
+
+    public void setGroupBy(String groupBy) {
+        this.groupBy = groupBy;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
     }
 }
