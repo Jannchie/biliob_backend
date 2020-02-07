932b7ac1f736402fd242573246b665f9b623ed20
jannchie <jannchie@gmail.com>
2020-02-07 11:46:14 +0900

    UPDATE: deploy scripy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
+source /etc/profile
+cd ~/biliob_backend
+mvn install
 pid=$(ps -ef | grep biliob | grep .jar | grep -v grep | awk '{print $2}')
 if [[ -n ${pid} ]]
 then
   kill -9 ${pid}
 fi
-source /etc/profile
-cd ~/biliob_backend
-mvn install
 nohup java -jar ./target/biliob*.jar 1>log.out 2>&1 &
