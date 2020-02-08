60b6c1f3372e88ab50c8d545781baa119c6cbe5d
jannchie <jannchie@gmail.com>
2020-02-08 13:07:51 +0800

    UPDATE: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source /etc/profile
 cd ~/biliob_backend
-mvn -Dtest package
+mvn -DfailIfNoTests=false -Dtest package
 pid=$(ps -ef | grep biliob | grep .jar | grep -v grep | awk '{print $2}')
 if [[ -n ${pid} ]]
 then
