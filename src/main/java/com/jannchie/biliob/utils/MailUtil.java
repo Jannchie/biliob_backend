65f3631b6bfb28c39b11e545c12cae6bb8ca573b
jannchie <jannchie@gmail.com>
2020-02-27 19:59:57 +0900

    FIX: Mail

--- src/main/java/com/jannchie/biliob/utils/MailUtil.java
+++ src/main/java/com/jannchie/biliob/utils/MailUtil.java
@@ -49,12 +49,12 @@ public class MailUtil {
         return checkStatus;
     }
 
-    public ResponseEntity sendActivationCode(String receiver) {
+    public ResponseEntity<?> sendActivationCode(String receiver) {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper;
         if (mongoTemplate.exists(Query.query(Criteria.where("mail").is(receiver)), "user")) {
             return new ResponseEntity<>(
-                    new Result(ResultEnum.MAIL_HAD_BEEN_REGISTERED), HttpStatus.NOT_ACCEPTABLE);
+                    new Result<>(ResultEnum.MAIL_HAD_BEEN_REGISTERED), HttpStatus.NOT_ACCEPTABLE);
         }
 
         try {
@@ -64,16 +64,7 @@ public class MailUtil {
             helper.setSubject("BiliOB观测者 - 邮箱验证");
             String code = String.valueOf((int) ((Math.random() * 9 + 1) * 100000));
             String temp =
-                    "<div style=\"margin: auto;text-align: center; color: dimgray; max-width: 800px\">\n"
-                            + "    <div>\n"
-                            + "        <img src=\"https://www.biliob.com/img/icons/android-chrome-192x192.png\">\n"
-                            + "    </div>\n"
-                            + "    感谢加入我们，观测者！\n"
-                            + "    <h2 style=\"color: dimgray\">您获得的识别码为</h2>\n"
-                            + "    <h1 style=\"color:dodgerblue\"> %s </h1>\n"
-                            + "    若非本人操作，请忽略该邮件吧\n"
-                            + "    <p style=\"text-align: end\">From Jannchie见齐</p>\n"
-                            + "</div>";
+                    "<div style=\"clear: both;\"><span style=\"font-family: simsun, STSongti-SC-Regular;\">BiliOB观测者接收到了您的请求。</span><br></div><div style=\"clear: both;\"><div style=\"margin: 0px; padding: 0px; border: 0px; outline: 0px; color: rgb(0, 0, 0); font-family: Tahoma, Arial, STHeiti, SimSun; font-size: 14px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; text-align: start; text-indent: 0px; text-transform: none; text-decoration-style: initial; text-decoration-color: initial; clear: both;\"><div class=\" __aliyun_node_has_bgcolor\" style=\"margin: 0px; padding: 0px; border: 0px; outline: 0px; font-variant-ligatures: normal; font-variant-caps: normal; text-align: start; text-indent: 0px; background-color: rgb(255, 255, 255); text-decoration-style: initial; text-decoration-color: initial; clear: both;\"><span class=\" __aliyun_node_has_color\" style=\"margin: 0px; padding: 0px; border: 0px; outline: 0px; color: rgb(0, 0, 0); font-size: 14px; font-style: normal; font-weight: 400; text-transform: none; font-family: simsun, STSongti-SC-Regular;\">请妥善使用这段代码：</span></div><div class=\" __aliyun_node_has_bgcolor\" style=\"margin: 0px; padding: 0px; border: 0px; outline: 0px; font-variant-ligatures: normal; font-variant-caps: normal; text-align: start; text-indent: 0px; background-color: rgb(255, 255, 255); text-decoration-style: initial; text-decoration-color: initial; clear: both;\"><span class=\" __aliyun_node_has_color\" style=\"margin: 0px; padding: 0px; border: 0px; outline: 0px; font-size: 14px; font-style: normal; text-transform: none; color: rgb(84, 141, 212); font-weight: 700; font-family: &quot;arial black&quot;;\">%s</span></div><div class=\" __aliyun_node_has_bgcolor\" style=\"margin: 0px; padding: 0px; border: 0px; outline: 0px; font-variant-ligatures: normal; font-variant-caps: normal; text-align: start; text-indent: 0px; background-color: rgb(255, 255, 255); text-decoration-style: initial; text-decoration-color: initial; clear: both;\"><span class=\" __aliyun_node_has_color\" style=\"margin: 0px; padding: 0px; border: 0px; outline: 0px; color: rgb(0, 0, 0); font-size: 14px; font-style: normal; font-weight: 400; text-transform: none; font-family: simsun, STSongti-SC-Regular;\">——观测站的管理员：Jannchie见齐</span></div></div></div>";
             String text = String.format(temp, code);
             helper.setText(text, true);
             mongoTemplate.upsert(
@@ -82,8 +73,8 @@ public class MailUtil {
                     MailUtil.ACTIVATION_CODE_COLLECTION);
             mailSender.send(message);
         } catch (Exception e) {
-            return new ResponseEntity<>(new Result(ResultEnum.SEND_MAIL_FAILED), HttpStatus.OK);
+            return new ResponseEntity<>(new Result<>(ResultEnum.SEND_MAIL_FAILED), HttpStatus.OK);
         }
-        return new ResponseEntity<>(new Result(ResultEnum.SUCCEED), HttpStatus.OK);
+        return new ResponseEntity<>(new Result<>(ResultEnum.SUCCEED), HttpStatus.OK);
     }
 }
