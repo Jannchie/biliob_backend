af5344c56c50573d203fc2bd25e3e793c1d2faba
jannchie <jannchie@gmail.com>
2020-08-05 11:19:43 +0900

    Update: Watch Dog

--- watchdog.sh
+++ watchdog.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 source /etc/profile
 cd ~/biliob_backend
-git checkout master
-git pull
-mvn clean
-mvn -DfailIfNoTests=false -Dtest package
 pid=$(ps -ef | grep biliob | grep .jar | grep -v grep | awk '{print $2}')
 if [[ $pid == "" ]]
 then
   echo "Cannot Find"
+  git checkout master
+  git pull
+  mvn clean
+  mvn -DfailIfNoTests=false -Dtest package
   nohup java -jar ./target/biliob*.jar 1>log.out 2>&1 &
 else
   echo "Finded"
