ea43b2670813045e8c4c10b45239969f54ce2207
jannchie <jannchie@gmail.com>
2020-08-27 19:08:31 +0900

    Add: User Opinion Class.

--- src/main/java/com/jannchie/biliob/constant/UserOpinion.java
+++ src/main/java/com/jannchie/biliob/constant/UserOpinion.java
@@ -8,7 +8,17 @@ public enum UserOpinion {
     /**
      * 赞成、反对、弃权
      */
-    IN_FAVOR,
-    AGAINST,
-    ABSTENTION
+    IN_FAVOR((byte) 1),
+    AGAINST((byte) 2),
+    ABSTENTION((byte) 3);
+
+    private final Byte value;
+
+    UserOpinion(Byte value) {
+        this.value = value;
+    }
+
+    public Byte getValue() {
+        return value;
+    }
 }
