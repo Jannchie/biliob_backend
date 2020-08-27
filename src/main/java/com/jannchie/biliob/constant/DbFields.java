27a4f397fa3ae6432a24c7e2bd79255088c926c2
jannchie <jannchie@gmail.com>
2020-08-27 19:02:28 +0900

    Add: Database Field Constants.

---
+++ src/main/java/com/jannchie/biliob/constant/DbFields.java
@@ -0,0 +1,21 @@
+package com.jannchie.biliob.constant;
+
+/**
+ * @author Jannchie
+ */
+public class DbFields {
+    public final static String
+            NAME = "name",
+            CREATOR_ID = "creator._id",
+            USER_ID = "user._id",
+            ID = "_id",
+            VOTES = "votes",
+            OPINION = "opinion",
+            AGENDA_ID = "agendaId",
+            AGAINST_COUNT = "againstCount",
+            FAVOR_COUNT = "favorCount",
+            AGAINST_SCORE = "againstScore",
+            STATE = "state",
+            FINISH_TIME = "finishTime",
+            FAVOR_SCORE = "favorScore";
+}
