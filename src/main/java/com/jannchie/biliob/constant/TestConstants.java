355ecae8c7dbede9f10a5c9536f7b3d1d69b4143
jannchie <jannchie@gmail.com>
2020-08-27 19:02:47 +0900

    Add: Test Constants.

---
+++ src/main/java/com/jannchie/biliob/constant/TestConstants.java
@@ -0,0 +1,9 @@
+package com.jannchie.biliob.constant;
+
+/**
+ * @author Jannchie
+ */
+public class TestConstants {
+    public final static String TEST_USER_NAME = "test";
+    public final static String NORMAL_USER_NAME = "biliob";
+}
