7821c2a74baeca8654deae75f965bed50391b3bd
jannchie <jannchie@gmail.com>
2020-10-15 15:13:33 +0900

    Update: Dependency

--- src/main/java/com/jannchie/biliob/BiliobApplication.java
+++ src/main/java/com/jannchie/biliob/BiliobApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
 
 /**
  * @author jannchie
  */
 @EnableCaching
 @EnableScheduling
+@EnableMongoHttpSession(maxInactiveIntervalInSeconds = 2592000)
 @SpringBootApplication
 @ConditionalOnProperty(
         value = "app.scheduling.enable", havingValue = "true", matchIfMissing = true
