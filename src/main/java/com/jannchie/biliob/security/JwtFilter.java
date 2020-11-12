c8707f970bc27544701016008c176e419ee14fe3
jannchie <jannchie@gmail.com>
2020-11-12 20:26:27 +0900

    Fix: Token

--- src/main/java/com/jannchie/biliob/security/JwtFilter.java
+++ src/main/java/com/jannchie/biliob/security/JwtFilter.java
@@ -2,6 +2,8 @@ package com.jannchie.biliob.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
@@ -23,6 +25,8 @@ import java.util.Collection;
  */
 public class JwtFilter extends BasicAuthenticationFilter {
 
+    private static final Logger logger = LogManager.getLogger();
+
     public JwtFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
     }
@@ -34,14 +38,19 @@ public class JwtFilter extends BasicAuthenticationFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         String token = request.getHeader("token");
-        if (token != null) {
-            DecodedJWT jwt = JWT.decode(token);
-            String name = jwt.getClaim("name").asString();
-            String role = jwt.getClaim("role").asString();
-            String password = jwt.getClaim("password").asString();
-            Collection<GrantedAuthority> authorityCollection = new ArrayList<>();
-            authorityCollection.add(new SimpleGrantedAuthority(role));
-            SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(name, password, authorityCollection));
+        try {
+
+            if (token != null) {
+                DecodedJWT jwt = JWT.decode(token);
+                String name = jwt.getClaim("name").asString();
+                String role = jwt.getClaim("role").asString();
+                String password = jwt.getClaim("password").asString();
+                Collection<GrantedAuthority> authorityCollection = new ArrayList<>();
+                authorityCollection.add(new SimpleGrantedAuthority(role));
+                SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(name, password, authorityCollection));
+            }
+        } catch (Exception e) {
+            logger.info("验证token失败");
         }
         chain.doFilter(request, response);
     }
